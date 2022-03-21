1416b49ad7b851ec6d32f8e4bdcb954e42da708f
Leyon <sgirad86@gmail.com>
2022-03-21 19:27:16 +0000

    fixed_All

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
     theme: {
         extend: {
             screens: {
+                'xs': '480px',
+                'sm': '600px',
+                'md': '960px',
+                'lg': '1280px',
+                'xl': '1920px',
                 'print': { 'raw': 'print' },
                 // => @media print { ... }
             }
